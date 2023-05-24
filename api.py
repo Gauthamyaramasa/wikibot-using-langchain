e1b1f7bfc2f13868473fcde58732dca4f1593bdc
Gautham Yaramasa <92468165+Gauthamyaramasa@users.noreply.github.com>
2023-05-24 20:07:51 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,25 @@
+# import streamlit for the app 
+import streamlit as st
+
+# Import langchain stuff 
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.llms import OpenAI
+
+# Import apikey and set it to the environment
+import os 
+
+# Set the apikey
+os.environ['OPENAI_API_KEY'] = "your-openai-api-key"
+
+# Initializing the LLM and the Toolset
+llm = OpenAI(temperature=0)
+tools = load_tools(["wikipedia"], llm=llm)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+st.title('Wikipedia Langchain Bot 🦜')
+# Collect the prompt from the user 
+input = st.text_input('Type Your Prompt Here')
+
+if input: 
+    text = agent.run(input)
+    st.write(text)
